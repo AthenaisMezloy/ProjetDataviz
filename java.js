bc0868089792cccb9403f86266422ff534b31ef0
AthenaisMezloy <115143120+AthenaisMezloy@users.noreply.github.com>
2022-11-08 10:31:58 +0100

    Update java.js

--- java.js
+++ java.js
@@ -290,18 +290,14 @@ var data = {
 // 3e Graphique
 
 var rate = d3.select(".rate");
-
 var	margin = {top: 30, right: 20, bottom: 30, left: 50},
 	width = 600 - margin.left - margin.right,
 	height = 270 - margin.top - margin.bottom;
 
-
 var	parseDate = d3.time.format("%d-%b-%y").parse;
-
 
 var	x = d3.time.scale().range([0, width]);
 var	y = d3.scale.linear().range([height, 0]);
-
 
 var	xAxis = d3.svg.axis().scale(x)
 	.orient("bottom").ticks(5);
@@ -309,19 +305,16 @@ var	xAxis = d3.svg.axis().scale(x)
 var	yAxis = d3.svg.axis().scale(y)
 	.orient("left").ticks(5);
 
-
 var	valueline = d3.svg.line()
 	.x(function(d) { return x(d.date); })
 	.y(function(d) { return y(d.Female); });
     
 
-var	svg = d3.select("rate-female")
-	.append("svg")
+var	svg = d3.select(".rate-female").append("svg")
 		.attr("width", width + margin.left + margin.right)
 		.attr("height", height + margin.top + margin.bottom)
 	  .append("g")
 		.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
 
 d3.json("data_JSON/rate.json", function(error, data) {
 	data.forEach(function(d) {
@@ -329,17 +322,15 @@ d3.json("data_JSON/rate.json", function(error, data) {
 		d.Female = +d.Female;
 	});
 
-	
 	x.domain(d3.extent(data, function(d) { return d.date; }));
 	y.domain([0, d3.max(data, function(d) { return d.Female; })]);
 
-	
 	svg.append("path")		
 		.attr("class", "line")
 		.attr("d", valueline(data));
 		
 		
-	svg	
+	svg		
 		.selectAll("circle")
 		.data(data)
 		.enter()
@@ -358,11 +349,11 @@ d3.json("data_JSON/rate.json", function(error, data) {
   
 });
 		
+
 	svg.append("g")			
 		.attr("class", "x axis")
 		.attr("transform", "translate(0," + height + ")")
 		.call(xAxis);
-
 
 	svg.append("g")			
 		.attr("class", "y axis")
