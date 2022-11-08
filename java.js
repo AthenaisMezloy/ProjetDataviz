995091e48ea2b9a960930b692ed555da84d135d8
AthenaisMezloy <115143120+AthenaisMezloy@users.noreply.github.com>
2022-11-08 09:52:35 +0100

    Update java.js

--- java.js
+++ java.js
@@ -290,31 +290,31 @@ var data = {
 // 3e Graphique
 
 var rate = d3.select(".rate");
-// Set the dimensions of the canvas / graph
+
 var	margin = {top: 30, right: 20, bottom: 30, left: 50},
 	width = 600 - margin.left - margin.right,
 	height = 270 - margin.top - margin.bottom;
 
-// Parse the date / time
+
 var	parseDate = d3.time.format("%d-%b-%y").parse;
 
-// Set the ranges
+
 var	x = d3.time.scale().range([0, width]);
 var	y = d3.scale.linear().range([height, 0]);
 
-// Define the axes
+
 var	xAxis = d3.svg.axis().scale(x)
 	.orient("bottom").ticks(5);
 
 var	yAxis = d3.svg.axis().scale(y)
 	.orient("left").ticks(5);
 
-// Define the line
+
 var	valueline = d3.svg.line()
 	.x(function(d) { return x(d.date); })
 	.y(function(d) { return y(d.Female); });
     
-// Adds the svg canvas
+
 var	svg = d3.select("main")
 	.append("svg")
 		.attr("width", width + margin.left + margin.right)
@@ -322,24 +322,24 @@ var	svg = d3.select("main")
 	  .append("g")
 		.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-// Get the data
+
 d3.json("data_JSON/rate.json", function(error, data) {
 	data.forEach(function(d) {
 		d.date = parseDate(d.date);
 		d.Female = +d.Female;
 	});
 
-	// Scale the range of the data
+	
 	x.domain(d3.extent(data, function(d) { return d.date; }));
 	y.domain([0, d3.max(data, function(d) { return d.Female; })]);
 
-	// Add the valueline path.
-	svg.append("path")		// Add the valueline path.
+	
+	svg.append("path")		
 		.attr("class", "line")
 		.attr("d", valueline(data));
 		
-		// Add the valueline path.
-	svg		// Add the valueline path.
+		
+	svg	
 		.selectAll("circle")
 		.data(data)
 		.enter()
@@ -358,14 +358,13 @@ d3.json("data_JSON/rate.json", function(error, data) {
   
 });
 		
-	// Add the X Axis
-	svg.append("g")			// Add the X Axis
+	svg.append("g")			
 		.attr("class", "x axis")
 		.attr("transform", "translate(0," + height + ")")
 		.call(xAxis);
 
-	// Add the Y Axis
-	svg.append("g")			// Add the Y Axis
+
+	svg.append("g")			
 		.attr("class", "y axis")
 		.call(yAxis);
 
